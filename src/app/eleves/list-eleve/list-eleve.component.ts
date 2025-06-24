cfa1812617f65c4e13a344183f7264d3c40a4922
ahmadoubamba <ahmadoubambandour54@gmail.com>
2025-06-24 21:27:52 +0000

    modification du fichier navbar.component.ts

--- src/app/eleves/list-eleve/list-eleve.component.ts
+++ src/app/eleves/list-eleve/list-eleve.component.ts
@@ -1,64 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { error } from 'console';
 import { EleveServiceService } from '../../services/eleves/eleve.service';
-
 
 @Component({
   selector: 'app-list-eleve',
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './list-eleve.component.html',
   styleUrl: './list-eleve.component.css'
 })
-export class ListEleveComponent implements OnInit{
-   eleves:any
-   nombres_eleves:number = 0
+export class ListEleveComponent implements OnInit {
+  eleves: any = [];
+  nombres_eleves: number = 0;
 
+  constructor(private route: Router, private eleveService: EleveServiceService) {}
 
-  constructor(private route:Router,private eleveService: EleveServiceService){
-    console.log("constructeurr")
+  ngOnInit() {
+    this.loadEleves();
   }
-  ngOnInit(){
-   this.eleveService.getEleves().subscribe(
+
+  loadEleves() {
+    this.eleveService.getEleves().subscribe(
       (response) => {
-        this.eleves = response
-        console.log(this.eleves.length)
+        this.eleves = response;
       },
       (error) => {
-        console.log(error )
-      },
-    )
+        console.log(error);
+      }
+    );
+  }
 
-    
+  editEleve(editEleve: any) {
+    localStorage.setItem("curentEleve", JSON.stringify(editEleve));
+    localStorage.setItem("editEleve", "1");
+    return this.route.navigate(["/eleve/form-eleve"]);
   }
-  editEleve(editEleve:any){
-    console.log(editEleve)
-    editEleve = JSON.stringify(editEleve)
-    localStorage.setItem("curentEleve",editEleve)
-    localStorage.setItem("editEleve","1")
-    return this.route.navigate(["/eleve/form-eleve"])
 
+  addEleve() {
+    localStorage.setItem("editEleve", "0");
+    return this.route.navigate(["/eleve/form-eleve"]);
   }
-  addEleve(){
-    localStorage.setItem("editEleve","0")
-    return this.route.navigate(["/eleve/form-eleve"])
-  }
-  deleteEleve(eleve:any){
-    let sup = confirm("Voulez vous suprimer")
-    if (sup){
+
+  deleteEleve(eleve: any) {
+    if (confirm("Voulez-vous supprimer ?")) {
       this.eleveService.deleteEleves(eleve.id).subscribe(
-        (response) => {
-          alert("enseignant bien supprimer")
-         
+        () => {
+          alert("Élève bien supprimé");
+          this.loadEleves(); 
         },
         (error) => {
-          console.log(error )
-        },
-      )
+          console.log(error);
+        }
+      );
     }
-    console.log(eleve)
   }
 }
--- src/app/enseignants/list-enseignant/list-enseignant.component.ts
+++ src/app/enseignants/list-enseignant/list-enseignant.component.ts
@@ -1,66 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { error } from 'console';
 import { EnseignantServiceService } from '../../services/enseignants/enseignant.service';
-
 
 @Component({
   selector: 'app-liste-enseignant',
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './list-enseignant.component.html',
   styleUrl: './list-enseignant.component.css'
 })
-export class ListEnseignantComponent implements OnInit{
-   enseignants:any
-   nombres_enseignant:number = 0
+export class ListEnseignantComponent implements OnInit {
+  enseignants: any = [];
+  nombres_enseignant: number = 0;
 
+  constructor(private route: Router, private enseignantService: EnseignantServiceService) {}
 
-  constructor(private route:Router,private enseignantService: EnseignantServiceService){
-    console.log("constructeurr")
+  ngOnInit() {
+    this.loadEnseignants();
   }
-  ngOnInit(){
-   this.enseignantService.getEnseignants().subscribe(
+
+  loadEnseignants() {
+    this.enseignantService.getEnseignants().subscribe(
       (response) => {
-        this.enseignants = response
-        console.log(this.enseignants.length)
+        this.enseignants = response;
       },
       (error) => {
-        console.log(error )
-      },
-    )
-    //this. nombres_eleve = this.classes.length
-    //console.log(this.nombres_eleve) 
-    
+        console.log(error);
+      }
+    );
   }
-  editEnseignant(editEnseignant:any){
-    console.log(editEnseignant)
-    editEnseignant = JSON.stringify(editEnseignant)
-    localStorage.setItem("curentEnseignant",editEnseignant)
-    localStorage.setItem("editEnseignant","1")
-    return this.route.navigate(["/enseignant/form-enseignant"])
 
+  editEnseignant(editEnseignant: any) {
+    localStorage.setItem("curentEnseignant", JSON.stringify(editEnseignant));
+    localStorage.setItem("editEnseignant", "1");
+    return this.route.navigate(["/enseignant/form-enseignant"]);
   }
-  addEnseignant(){
-    //console.log("add initialiser")
-    localStorage.setItem("editEnseignant","0")
-    return this.route.navigate(["/enseignant/form-enseignant"])
+
+  addEnseignant() {
+    localStorage.setItem("editEnseignant", "0");
+    return this.route.navigate(["/enseignant/form-enseignant"]);
   }
-  deleteEnseignant(enseignant:any){
-    let sup = confirm("Voulez vous suprimer")
-    if (sup){
+
+  deleteEnseignant(enseignant: any) {
+    if (confirm("Voulez-vous supprimer ?")) {
       this.enseignantService.deleteEnseignants(enseignant.id).subscribe(
-        (response) => {
-          alert("enseignant bien supprimer")
-         
+        () => {
+          alert("Enseignant bien supprimé");
+          this.loadEnseignants();
         },
         (error) => {
-          console.log(error )
-        },
-      )
+          console.log(error);
+        }
+      );
     }
-    console.log(enseignant)
   }
 }
