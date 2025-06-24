5c28782cd196bb0f14d8d25fed7f613ab664e21a
ahmadoubamba <ahmadoubambandour54@gmail.com>
2025-06-24 10:24:42 +0000

    initialisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HttpClient} from "@angular/common/http";
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { HomeComponent } from "./home/home.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'angular_project';
-
-  constructor( private httpClient: HttpClient) {
-
-  }
-
-  ngOnInit() {
-    this.httpClient.get("http://localhost:3000/eleves").subscribe(
-        (response) =>{
-          console.log(response);
-        }, (error) => {
-          console.log(error);
-        }
-    )
-  }
-
-
+export class AppComponent {
+  title = 'gestion-scolaire';
 }
