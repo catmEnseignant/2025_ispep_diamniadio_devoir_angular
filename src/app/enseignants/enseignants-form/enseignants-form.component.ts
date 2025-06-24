e6cc0425a417f12a9470840512ce18a9c7a751d4
hassanatou-diallo <hassanatoudiallo772@gmail.com>
2025-06-24 21:14:35 +0000

    modification des fichiers

--- src/app/enseignants/enseignants-form/enseignants-form.component.ts
+++ src/app/enseignants/enseignants-form/enseignants-form.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {  FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { EnseignantsServiceService } from '../../services/enseignants/enseignants-service.service';
+import { Router } from '@angular/router'; 
+
+
 
 @Component({
   selector: 'app-enseignants-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './enseignants-form.component.html',
   styleUrl: './enseignants-form.component.css'
 })
-export class EnseignantsFormComponent {
+export class EnseignantsFormComponent implements OnInit{
+
+  enseignantsform:FormGroup
+
+  enseignantsedit:any
+  isedit:any
+  
+
+  constructor(private fb:FormBuilder, private enseignantsService: EnseignantsServiceService, private route:Router){
+    
+    this.enseignantsform= this.fb.group({})
+  }
+  ngOnInit(): void{
+    this.isedit = localStorage.getItem("editEnseignants")
+    if(this.isedit == 1){
+      this.enseignantsedit = localStorage.getItem('curentClasse')
+      this.enseignantsedit=JSON.parse(this.enseignantsedit)
+      console.log(this.enseignantsedit.nom)
+      console.log(this.enseignantsedit)
+      this.enseignantsform= this.fb.group({
+        nom:[this.enseignantsedit.nom],
+        niveau:[this.enseignantsedit.niveau],
+        serie:[this.enseignantsedit.serie],
+        nombres_eleve:[this.enseignantsedit.nombres_enseignant]
+      })
+    }
+    else{
+      this.enseignantsform= this.fb.group({
+        nom:[''],
+        prenom:[''],
+        adresse:[''],
+        telephone:['']
+      })
+      
+    }
+    console.log(this.isedit)
+  }
+  submitEnseignants(){
+    if(this.isedit == 1){
+      this.enseignantsService.updateEnseignants(this.enseignantsedit.id,this.enseignantsform.value).subscribe(
+        (response) => {
+          return this.route.navigate(["/enseignants/enseignants-list"]);
+        },
+        (error) =>{
+          console.log(error)
+        }
+      )
+    }else{  
+        this.enseignantsService.storeEnseignants(this.enseignantsform.value).subscribe(
+          (response) => {
+            return this.route.navigate(["/enseignants/enseignants-list"]);
+          },
+          (error) =>{
+            console.log(error)
+          }
+        )
+    }  
+    //console.log(this.enseignantsform.value)
+  }
 
 }
--- src/app/enseignants/enseignants-list/enseignants-list.component.ts
+++ src/app/enseignants/enseignants-list/enseignants-list.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EnseignantsServiceService } from '../../services/enseignants/enseignants-service.service';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'console';
 
 @Component({
   selector: 'app-enseignants-list',
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './enseignants-list.component.html',
   styleUrl: './enseignants-list.component.css'
 })
-export class EnseignantsListComponent {
+export class EnseignantsListComponent implements OnInit{
+  enseignants:any
+  nombres_enseignant:number = 0
 
+
+  constructor(private route:Router,private enseignantsService: EnseignantsServiceService){
+    console.log("constructeurr")
+  }
+  ngOnInit(){
+    this.enseignantsService.getEnseignants().subscribe(
+      (response) => {
+        this.enseignants = response
+        console.log(this.enseignants.length)
+      },
+      (error) => {
+        console.log(error )
+      },
+    )
+    //this. nombres_eleve = this.classes.length
+    //console.log(this.nombres_eleve) 
+    
+  }
+  editEnseignants(editEnseignants:any){
+    console.log(editEnseignants)
+    editEnseignants = JSON.stringify(editEnseignants)
+    localStorage.setItem("curentEnseignants",editEnseignants)
+    localStorage.setItem("editEnseignants","1")
+    return this.route.navigate(["/enseignants/enseignants-form"])
+
+  }
+  addEnseignants(){
+    //console.log("add initialiser")
+    localStorage.setItem("editEnseignants","0")
+    return this.route.navigate(["/enseignants/enseignants-form"])
+  }
+  deleteEnseignants(enseignants:any){
+    let sup = confirm("Voulez vous suprimer")
+    if (sup){
+      this.enseignantsService.deleteEnseignants(enseignants.id).subscribe(
+        (response) => {
+          alert("enseignants bien supprimer")        
+        },
+        (error) => {
+          console.log(error )
+        },
+      )
+    }
+    console.log(enseignants)
+  }
 }
